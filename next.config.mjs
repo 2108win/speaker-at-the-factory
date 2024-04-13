3f07c4ea6db41e63956dd9e419d5c19194563b46
2108win <dangcapcuawin@gmail.com>
2024-04-14 05:32:45 +0700

    [FIX] delete config image not use

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "dummyimage.com",
-        port: "",
-        pathname: "",
-      },
-    ],
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
